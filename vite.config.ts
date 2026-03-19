d82181d0f488dfafc4104e1a438c44f812c16bda
hansaejo5-blip <hansaejo5@gmail.com>
2026-03-20 02:37:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/i-want-to-perfect/',
-  plugins: [react()],
-})
+    base: './',
+    plugins: [react()],
+  })
